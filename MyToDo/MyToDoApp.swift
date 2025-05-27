8a9015ca7405f620586ebc7cd3b76860e062ab52
VladimirS <serbinvova1337@gmail.com>
2025-05-27 19:17:22 +0300

    Initial Commit

---
+++ MyToDo/MyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoApp.swift
+//  MyToDo
+//
+//  Created by Владимир Сербин on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyToDoTests/MyToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoTests.swift
+//  MyToDoTests
+//
+//  Created by Владимир Сербин on 27.05.2025.
+//
+
+import Testing
+@testable import MyToDo
+
+struct MyToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
